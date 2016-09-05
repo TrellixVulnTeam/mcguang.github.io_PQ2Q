27911e1d9d9695ef8c541c97f1d5f73c41091b84
Mei CG <meicg@MeideMacBook-Pro.local>
2016-09-05 10:30:49 +0800

    add leetCode experiment-01

---
+++ leetCode_experiment/01_string_1/C/Solution.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int strStr(char* haystack, char* needle) {
+	if (haystack == NULL || needle == NULL) 
+		return -1;
+
+	const int len_h = strlen(haystack);
+	const int len_n = strlen(needle);
+	for (int i = 0; i < len_h - len_n + 1; i++){
+		int j = 0;
+		for (; j < len_n; j++){
+			if (haystack[i+j] != needle[j]){
+				break;
+			}
+		}
+		if (j == len_n) return i;
+	}
+
+	return -1;
+	
+}
+
+int main(int argc, char** argv ){
+	printf("result = %d\n",strStr("source","urc"));
+}
